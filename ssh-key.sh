42ebcb92410f54463b8a1584bd281f989c3045a0
Lucas Aponso <lucasaponso@outlook.com>
2022-12-13 13:02:48 +1100

    fixing a couple of errors

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 ssh-keygen -t ed25519 -C lucasaponso@outlook.com
 eval "$(ssh-agent -s)"
 FILE=~/.ssh/config
 if test -f "$FILE"; then
-    echo "Host *\n AddKeysToAgent yes \n IdentityFile ~/.ssh/id_ed25519 > ~/.ssh/config"
+    echo "Host * 
+		AddKeysToAgent yes 
+		IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
 else
 	touch ~/.ssh/config
-	echo "Host *\n AddKeysToAgent yes \n IdentityFile ~/.ssh/id_ed25519 > ~/.ssh/config"
+	echo "Host * 
+		AddKeysToAgent yes  
+		IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
 
 fi
 
-ssh-add ~/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519.pub
 cat ~/.ssh/ed25519.pub
 
