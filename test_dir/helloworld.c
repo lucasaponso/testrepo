e9d45c3f5f326e1f913cd3b8c28c350016618757
Lucas Aponso <lucasaponso@outlook.com>
2022-12-13 13:22:42 +1100

    printing memory address, and printing char

--- test_dir/helloworld.c
+++ test_dir/helloworld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main() {
+   int a = 2;
+   char str[] = "Lucas Aponso"; 
+   printf("%s\n", str); 
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Memory Address of a is: %p\n",&a);
    return 0;
 }
