b0cc69c5b0210bf661ccbb0b8d1dc6354c15af29
unknown <LucasPC@DESKTOP-5DQV4F7.avara.local>
2022-12-19 18:50:45 +1100

    adding a couple of files and deleting web pafe

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-##Introduction of program!!
-echo "Hello, I am a program that generates a ssh key that can be used for cloning github repo's!!"
-sleep 2s
-
 read -p "Give me ur email!!!" email_addr
 ssh-keygen -t ed25519 -C $email_addr
 eval "$(ssh-agent -s)"
 FILE=~/.ssh/config
+
 if test -f "$FILE"; then
     echo "Host * 
 		AddKeysToAgent yes 
@@ -17,13 +13,14 @@ else
 	echo "Host * 
 		AddKeysToAgent yes  
 		IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
-
 fi
 
 ssh-add ~/.ssh/id_ed25519.pub
 cat ~/.ssh/id_ed25519.pub
 echo "Would you like to remove the current directory: (Y/N)"
 read remove_pwd
+
+
 if [ [remove_pwd == 'Y' || remove_pwd == 'y'] ]
 then
 	rm -rf $(pwd)
