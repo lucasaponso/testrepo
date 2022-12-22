eb20655e64c9bbacae436ee58d9ad1f2ea54a6ee
Lucas Aponso <lucas.aponso@avara.local>
2022-12-22 19:17:01 +1100

    commit

--- install_dep.sh
+++ install_dep.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
-
-##PROGRAM STARTS HERE
 neofetch --ascii_distro archlinux
 echo $(date)
 umask_val=$(sudo su root -c umask)
 echo "umask = "$umask_val
-
-
 ##GETTING THE SCRIPT READY
 distro_type=$(cat /etc/*-release | grep DISTRIB_ID=Ubuntu)
 echo $distro_type
 echo "Using apt packet manager to install packages!!"
-
 if [[ $distro_type == "DISTRIB_ID=Ubuntu" ]]; then
      sudo apt update
      sudo apt upgrade -y
@@ -24,29 +18,14 @@ else
     echo "You most likely have an arch system, meaning package names may differ"
     echo "Unable to install packages!!"
 fi
-
-
-##TESTING NETWORK DISCOVERY
-
 echo "PINGING UNITS!!"
-
-
-##Avara Unit1
 HOST1=10.20.212.50
-##Avara Unit2
 HOST2=10.20.212.60
-##Avara Unit3
 HOST3=10.20.212.61
-##Avara Unit4
 HOST4=10.20.212.62
-##DarkHelmet
 HOST5=192.168.0.20
-##Linode Cloud Server
 HOST6=172.105.170.242
-
-##Testing Ping     
 myArray=("$HOST1" "$HOST2" "$HOST3" "$HOST4" "$HOST5" "$HOST6")
-
 for i in "${myArray[@]}"
 do
     sudo ping $i -c 1 >> /log/network.log
@@ -86,4 +65,3 @@ if [[ $git_commit == "Y" || $git_commit == "y" ]]
     then
         echo "Ok"
 fi
-
---
+++ ping_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HOST1=10.20.212.50
+HOST2=10.20.212.60
+HOST3=10.20.212.61
+HOST4=10.20.212.62
+
+HOST5=10.20.220.230
+HOST6=10.10.109.20
+HOST7=10.10.107.3
+HOST8=10.10.107.80
+
+HOST9=192.168.0.20
+HOST10=172.105.180.73
+myArray=("$HOST1" "$HOST2" "$HOST3" "$HOST4" "$HOST5" "$HOST6" "$HOST7" "$HOST8" "$HOST9" "$HOST10")
+for i in "${myArray[@]}"
+do sudo ping $i -c 1 >> /log/network.log
+    ping -c1 $i 1>/dev/null 2>/dev/null
+    SUCCESS=$?
+    if [ $SUCCESS -eq 0 ]
+    then
+        
+        echo "$i has replied"
+    
+    else
+        echo "$i didn't reply, check /log/network.log"
+    
+    fi
+    sudo nmap $i >> /log/security.log  
+    
+  
+done
