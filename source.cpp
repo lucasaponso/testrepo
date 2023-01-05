1a04bd6791dd38feec799a472cdf6d9e7195d481
Lucas Aponso <lucas.aponso@avara.local>
2023-01-05 11:52:53 +1100

    adding c++

---
+++ source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string cars[5] = {"10.20.212.50 -c 2 ", "10.20.212.60 -c 2", "10.20.212.61 -c 2", "10.20.212.62 -c 2", "10.20.220.230 -c 2"};
+  for (int i = 0; i < 5; i++) {
+    string str = "ping ";
+    str = str + cars[i];
+    const char *command = str.c_str();
+
+    cout << "Compiling file using " << command << endl;
+    system(command);
+  }
+  return 0;
+ }
+
